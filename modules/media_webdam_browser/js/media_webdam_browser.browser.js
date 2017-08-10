d7cdc16fe53c828574d309da6d264bf7d2e02040
Steven Wichers <steven.wichers@gmail.com>
2017-08-09 23:25:16 -0700

    Initial project commit.

---
+++ modules/media_webdam_browser/js/media_webdam_browser.browser.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+  var mediaMediaItemSelectToggle = function () {
+    // If the media item is clicked anywhere other than on the image itself
+    // check the checkbox. For the record, JS thinks this is wonky.
+    $('.media-item', this).bind('click', function (e) {
+      if ($(e.target).is('img, a')) {
+        return;
+      }
+      var checkbox = $(this).parent().find(':checkbox, :radio');
+      if (checkbox.is(':checked')) {
+        checkbox.attr('checked', false).change();
+      } else {
+        checkbox.attr('checked', true).change();
+      }
+    });
+
+    // Add an extra class to selected thumbnails.
+    $('.media-display-thumbnails :checkbox, .media-display-thumbnails :radio', this).each(function () {
+      var checkbox = $(this);
+      if (checkbox.is(':checked')) {
+        $(checkbox.parents('li').find('.media-item')).addClass('selected');
+      }
+
+      checkbox.bind('change.media', function () {
+        if (checkbox.is(':checked')) {
+          $(checkbox.parents('li').find('.media-item')).addClass('selected');
+        }
+        else {
+          $(checkbox.parents('li').find('.media-item')).removeClass('selected');
+        }
+      });
+    });
+  };
+
+  Drupal.behaviors.mediaWebdamBrowser = {
+    attach: function (context, settings) {
+
+      $('.media-webdam-browser-assets', context).once(mediaMediaItemSelectToggle);
+    }
+  };
+})(jQuery);
---
+++ modules/media_webdam_browser/templates/media-webdam-browser-asset.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Template implementation for assets displayed within the Media Browser plugin.
+ *
+ * Variables:
+ *  $asset: The asset object being rendered.
+ *  $browser_url: The current Media Browser URL.
+ *  $thumbnail: The thumbnail markup to use.
+ */
+?>
+<div class="media-item" data-asset-type="<?php echo $asset['type'] ?>" data-asset-id="<?php echo $asset['id'] ?>">
+  <div class="media-thumbnail">
+    <?php if ($asset['type'] == 'folder') : ?>
+      <a href="<?php print $browser_url; ?>"><?php echo $thumbnail?></a>
+    <?php else : ?>
+      <?php echo $thumbnail?>
+    <?php endif; ?>
+    <div class="label-wrapper">
+      <label class="media-filename">
+      <?php if ($asset['type'] == 'folder') : ?>
+        <a href="<?php print $browser_url; ?>"><?php echo $asset['name'] ?></a>
+      <?php else : ?>
+        <?php echo $asset['name'] ?>
+      <?php endif; ?>
+      </label>
+    </div>
+  </div>
+</div>
