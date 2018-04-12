093186f4c825f28646fe7895637a1f5da9316600
Steven Wichers <swichers@users.noreply.github.com>
2018-04-12 12:06:38 -0700

    DAM-91: Implement additional API endpoints. (#24)

--- src/Client.php
+++ src/Client.php
@@ -9,8 +9,10 @@
 namespace Drupal\media_acquiadam;
 
 use cweagans\webdam\Client as OriginalClient;
+use cweagans\webdam\Entity\Asset;
 use cweagans\webdam\Exception\InvalidCredentialsException;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\RequestOptions;
 
 class Client extends OriginalClient {
 
@@ -164,7 +166,7 @@ class Client extends OriginalClient {
    */
   public function getActiveXmpFields() {
     $this->checkAuth();
-    
+
     $response = $this->client->request(
       'GET',
       $this->baseUrl . '/metadataschemas/xmp?full=1',
@@ -185,6 +187,206 @@ class Client extends OriginalClient {
     }
 
     return $metadata;
+  }
+
+  /**
+   * Queue custom asset conversions for download.
+   *
+   * This is a 2 step process:
+   *   1. Queue assets
+   *   2. Download From Queue
+   *
+   * This step will allow users to queue an asset for download by specifying an
+   * AssetID and a Preset ID or custom conversion parameters. If a valid
+   * PresetID is defined, the other conversions parameters will be ignored
+   * (format, resolution, size, orientation, colorspace).
+   *
+   * @param array|int $assetIDs
+   *   A single or list of asset IDs.
+   *
+   * @param $options
+   *   Asset preset or conversion options.
+   *
+   * @return array
+   *   An array of response data.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   * @throws \cweagans\webdam\Exception\InvalidCredentialsException
+   */
+  public function queueAssetDownload($assetIDs, $options) {
+    $this->checkAuth();
+
+    if (!is_array($assetIDs)) {
+      $assetIDs = [$assetIDs];
+    }
+
+    $data = ['items' => []];
+    foreach ($assetIDs as $assetID) {
+      $data['items'][] = ['id' => $assetID] + $options;
+    }
+
+    $response = $this->client->request(
+      'POST',
+      $this->baseUrl . '/assets/queuedownload',
+      [
+        'headers' => $this->getDefaultHeaders(),
+        RequestOptions::JSON => $data,
+      ]
+    );
+    $response = json_decode((string) $response->getBody(), TRUE);
+
+    return $response;
+  }
+
+  /**
+   * Gets asset download queue information.
+   *
+   * This is a 2 step process:
+   *   1. Queue assets
+   *   2. Download From Queue
+   *
+   * This step will allow users to download the queued asset using the download
+   * key returned from step1 (Queue asset process). The output of this step will
+   * be a download URL to the asset or the download status, if the asset is not
+   * ready for download.
+   *
+   * @param string $downloadKey
+   *   The download key to check the status of.
+   *
+   * @return array
+   *   An array of response data.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   * @throws \cweagans\webdam\Exception\InvalidCredentialsException
+   */
+  public function downloadFromQueue($downloadKey) {
+    $this->checkAuth();
+
+    $response = $this->client->request(
+      'GET',
+      $this->baseUrl . '/downloadfromqueue/' . $downloadKey,
+      ['headers' => $this->getDefaultHeaders()]
+    );
+
+    $response = json_decode((string) $response->getBody(), TRUE);
+
+    return $response;
+  }
+
+  /**
+   * Edit an asset.
+   *
+   * If an asset is uploaded and its required fields are not filled in, the
+   * asset is in onhold status and cannot be activated until all required fields
+   * are supplied. Any attempt to change the status to 'active' for assets that
+   * still require metadata will return back 409.
+   *
+   * @param int $assetID
+   *   The asset to edit.
+   * @param array $data
+   *   An array of values to set.
+   *    filename       string  The new filename for the asset.
+   *    status         string  The new status of the asset. Either active or
+   *                           inactive.
+   *    name           string  The new name for the asset.
+   *    description    string  The new description of the asset.
+   *    folder         long    The id of the folder to move asset to.
+   *    thumbnail_ttl  string  Time to live for thumbnails
+   *                             Default: Set by the account admin
+   *                             Values: '+3 min', '+15 min', '+2 hours',
+   *                             '+1 day', '+2 weeks', 'no-expiration'
+   *
+   * @return \cweagans\webdam\Entity\Asset|bool
+   *   An asset object on success, or FALSE on failure.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   * @throws \cweagans\webdam\Exception\InvalidCredentialsException
+   */
+  public function editAsset($assetID, array $data) {
+    $this->checkAuth();
+
+    $response = $this->client->request(
+      'PUT',
+      $this->baseUrl . '/assets/' . $assetID,
+      [
+        'headers' => $this->getDefaultHeaders(),
+        RequestOptions::JSON => $data,
+      ]
+    );
+
+    if (409 == $response->getStatusCode()) {
+      return FALSE;
+    }
+
+    $asset = Asset::fromJson((string) $response->getBody());
+
+    return $asset;
+  }
+
+  /**
+   * Edit asset XMP metadata.
+   *
+   * @param int $assetID
+   *   The asset to edit XMP metadata for.
+   * @param array $data
+   *   A key value array of metadata to edit.
+   *
+   * @return array
+   *   The metadata of the asset.
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   * @throws \cweagans\webdam\Exception\InvalidCredentialsException
+   */
+  public function editAssetXmpMetadata($assetID, array $data) {
+    $this->checkAuth();
+
+    $data['type'] = 'assetxmp';
+
+    $response = $this->client->request(
+      'PUT',
+      $this->baseUrl . '/assets/' . $assetID . '/metadatas/xmp',
+      [
+        'headers' => $this->getDefaultHeaders(),
+        RequestOptions::JSON => $data,
+      ]
+    );
+
+    $response = json_decode((string) $response->getBody(), TRUE);
+
+    return $response;
+  }
+
+  /**
+   * Uploads file to Webdam AWS S3.
+   *
+   * @param mixed $presignedUrl
+   *   The presigned URL we got in previous step from AWS.
+   * @param string $file_uri
+   *   The file URI.
+   * @param string $file_type
+   *   The File Content Type.
+   *
+   * @return array
+   *   Response Status 100 / 200
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   * @throws \cweagans\webdam\Exception\InvalidCredentialsException
+   */
+  protected function uploadPresigned($presignedUrl, $file_uri, $file_type) {
+    $this->checkAuth();
+
+    $file = fopen($file_uri, 'r');
+    $response = $this->client->request(
+      "PUT",
+      $presignedUrl, [
+      'headers' => ['Content-Type' => $file_type],
+      'body' => stream_get_contents($file),
+      RequestOptions::TIMEOUT => 0,
+    ]);
+
+    return [
+      'status' => json_decode($response->getStatusCode(), TRUE),
+    ];
+
   }
 
 }
