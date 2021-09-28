ec76cb82e8cc5f55306f77195e952f3119b132ae
vbouchet <vincent.bouchet.31@gmail.com>
2021-09-28 16:52:26 +0200

    WDIM-35: Use queue for registration link process.

---
+++ src/Plugin/QueueWorker/IntegrationLinkReport.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\acquiadam\Plugin\QueueWorker;
+
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\acquiadam\Client;
+
+/**
+ * Report Acquia DAM asset usage via integration links.
+ *
+ * @QueueWorker (
+ *   id = "acquiadam_integration_link_report",
+ *   title = @Translation("Acquia DAM Integration Link Report"),
+ *   cron = {"time" = 30}
+ * )
+ */
+class IntegrationLinkReport extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Acquia DAM client factory.
+   *
+   * @var \Drupal\acquiadam\Client
+   */
+  protected $client;
+
+  /**
+   * Drupal logger channel service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $loggerChannel;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Client $client, LoggerChannelInterface $loggerChannel) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->client = $client;
+    $this->loggerChannel = $loggerChannel;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('acquiadam.client'),
+      $container->get('logger.factory')->get('acquiadam')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return bool
+   *   TRUE if the integration link properly reported, FALSE - otherwise.
+   */
+  public function processItem($data) {
+    // If we don't have all the data to report the usage, don't go further.
+    if (empty($data['mid']) || empty($data['asset_uuid']) || empty($data['url'])) {
+      return FALSE;
+    }
+
+    try {
+      $post = [
+        'assetUuid' => $data['asset_uuid'],
+        'description' => "Acquia DAM module: Asset referenced from Media Entity {$data['mid']}",
+        'url' => $data['url'],
+      ];
+
+      $this->client->registerIntegrationLink($post);
+    } catch (\Exception $x) {
+      $this->loggerChannel->error(
+        'Error trying to create the integration link for @media_id',
+        ['@media_id' => $data['mid']]
+      );
+      return FALSE;
+    }
+
+    return TRUE;
+  }
+}
